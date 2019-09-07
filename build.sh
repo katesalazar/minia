36aa3d8d7f6288aea5d324d40d5836356b5dac91
katesalazar <mercedes.catherine.salazar@gmail.com>
2019-09-07 22:53:47 +0200

    wip

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 
 set -x
 
-uname -a
+# uname -a
 # expecting `Darwin`
 
-markdown -o minia.html minia.md
+./doc/bin/generate.py --type minia.org \
+        <./doc/src/doc.mmd >./doc/out_minia.org/doc.md
+markdown <./doc/out_minia.org/doc.md >./doc/out_minia.org/doc.html
+./doc/bin/generate.py --type GitHub \
+        <./doc/src/doc.mmd >./doc/out_GitHub/doc.md
