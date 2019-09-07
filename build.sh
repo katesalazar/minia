b3e5b32d92813e8d28c4a2a5ae6f06e4114f7047
katesalazar <mercedes.catherine.salazar@gmail.com>
2019-09-07 22:58:13 +0200

    wip

--- build.sh
+++ build.sh
@@ -5,8 +5,13 @@ set -x
 # uname -a
 # expecting `Darwin`
 
+mkdir ./doc/out_minia.org/
+
 ./doc/bin/generate.py --type minia.org \
         <./doc/src/doc.mmd >./doc/out_minia.org/doc.md
 markdown <./doc/out_minia.org/doc.md >./doc/out_minia.org/doc.html
+
+mkdir ./doc/out_GitHub/
+
 ./doc/bin/generate.py --type GitHub \
         <./doc/src/doc.mmd >./doc/out_GitHub/doc.md
